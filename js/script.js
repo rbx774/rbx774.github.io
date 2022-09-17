f650b707a95ea2d011a23810897c846a7950aebe
Norbert Roettger <norbert.roettger@gmail.com>
2022-09-17 20:31:05 +0200

    PWA version
    converted to PWA

---
+++ js/script.js
@@ -0,0 +1,19 @@
+swURL ="./service-worker.js"
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
+
+function pageReady() {
+    console.log("Hi There ...") 
+}
+
